bffe45b0dd992bc48a7518e2e21c779b841ddeb6
Gabriel <gabriel_l_o@hotmail.com>
2020-12-05 18:45:57 -0300

    ajuste server js

--- Angular/server.js
+++ Angular/server.js
@@ -1,6 +1,5 @@
 const { prototype } = require("events");
 const express = require("express");
-const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -8,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/MaridoDeAluguel'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/dist/MaridoDeAluguel/intex.html');
+    res.sendFile(__dirname + '/dist/MaridoDeAluguel/index.html');
 });
 
 app.listen(PORT, () => {
